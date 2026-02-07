9a5509e23cb4027a7ac10fe3a6c2991d9b61ae1e
DrBopoH <lecrowz666@gmail.com>
2026-02-07 20:54:17 +0200

    chunk generation

--- project/Source/Main/world.gd
+++ project/Source/Main/world.gd
@@ -3,15 +3,20 @@ class_name world
 
 var entity: CameraFreeMovement
 
-var block: MeshInstance
+#var block: MeshInstance
+var chunks: chunk
 
-var texture: Texture = load("res://Source/Assets/Textures/Blocks/stone.png")
+#var texture: Texture = load("res://Source/Assets/Textures/Blocks/stone.png")
 
 func _ready():
-	block = MeshInstance.new()
-	block.mesh = MeshGen.create_textured_cube(Vector3(1, 1, 1), texture, true)
+	#block = MeshInstance.new()
+	#block.mesh = VoxelGenerator.create_textured_cube(Vector3(1, 1, 1), texture, true)
 	
-	add_child(block)
+	#add_child(block)
+	
+	chunks = chunk.new()
+	
+	add_child(chunks)
 	
 	entity = CameraFreeMovement.new()
 	entity.camuscon.camera.current = true
